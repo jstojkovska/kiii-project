85cf0d12bad8d35835572e055d0a9716e92dae4a
Jana <janastoj@hotmail.com>
2025-06-29 22:30:43 +0200

    Use env vars in application.properties

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost/api",
+    baseURL: "http://localhost/api"git commit -m "Changed heading from h5 to h6",
     headers: {
         "Content-Type": "application/json"
     }
